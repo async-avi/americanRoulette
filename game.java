17b9054ab50b82fc4bd974a1d394a7e1c82e0624
async-avi <work.abhinav@yahoo.com>
2026-03-06 21:32:13 +0530

    Template ready

---
+++ game.java
@@ -0,0 +1,10 @@
+class Player{
+    
+}
+public class game {
+    public static void main(String[] args) {
+        System.out.println("Welcome to American Roulette!");
+        System.out.println("Please place your bets.");
+        // Additional game logic would go here
+    }
+}
